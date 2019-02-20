be3c982674eafc1cf02ef188eef11fc587a15bdb
tusmester <tusmester@gmail.com>
2019-02-20 17:21:57 +0100

    Convert Local and Central projects to NetStandard.

--- src/SenseNet.Search.Lucene29.Centralized.Common/Properties/AssemblyInfo.cs
+++ src/SenseNet.Search.Lucene29.Centralized.Common/Properties/AssemblyInfo.cs
@@ -4,22 +4,7 @@ using System.Runtime.InteropServices;
 
 [assembly: InternalsVisibleTo("SenseNet.Search.Lucene29.Tests")]
 
-#if DEBUG
-[assembly: AssemblyTitle("SenseNet.Search.Lucene29.Centralized.Common (Debug)")]
-#else
-[assembly: AssemblyTitle("SenseNet.Search.Lucene29.Centralized.Common (Release)")]
-#endif
-
-[assembly: AssemblyDescription("Common library for the sensenet Centralized search engine implementation for Lucene29.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Sense/Net Inc.")]
-[assembly: AssemblyCopyright("Copyright © Sense/Net Inc.")]
-[assembly: AssemblyProduct("sensenet")]
 [assembly: AssemblyTrademark("Sense/Net Inc.")]
-[assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
-[assembly: AssemblyInformationalVersion("1.1.0")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("0352738e-fda0-4adf-934b-4f9ea7922152")]
--- tools/scripts/CreateNuGetPackages.ps1
+++ tools/scripts/CreateNuGetPackages.ps1
@@ -2,7 +2,3 @@
 
 # delete existing packages
 Remove-Item $PSScriptRoot\*.nupkg
-
-nuget pack $srcPath\SenseNet.Search.Lucene29.Common\SenseNet.Search.Lucene29.Common.csproj -properties Configuration=Release -OutputDirectory $PSScriptRoot
-nuget pack $srcPath\SenseNet.Search.Lucene29.Local\SenseNet.Search.Lucene29.Local.csproj -properties Configuration=Release -OutputDirectory $PSScriptRoot
-nuget pack $srcPath\SenseNet.Search.Lucene29.Centralized\SenseNet.Search.Lucene29.Centralized.csproj -properties Configuration=Release -OutputDirectory $PSScriptRoot
