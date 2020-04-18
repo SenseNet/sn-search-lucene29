b93c75a6aa78867ab69e850106da12440efe7d5e
tusmester <tusmester@gmail.com>
2020-04-18 09:41:06 +0200

    Feature/grpc service (#23)
    * Grpc search service prototype with empty methods.
    * Convert Centralized.Index project to netstandard.
    * Draft grpc search service implementation.
    * Add service setter api and convert Centralized.Common project to a standalone package.
    * Add GrpcClient project.
    * Add configuration, logging and tracong to the GrpcService project.
    * Dispose grpc channel on shutdown and add configure method for the feature.

---
+++ src/SenseNet.Search.Lucene29.Centralized.GrpcClient/GrpcClientExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Grpc.Net.Client;
+using SenseNet.Diagnostics;
+using SenseNet.Tools;
+
+namespace SenseNet.Search.Lucene29.Centralized.GrpcClient
+{
+    public static class GrpcClientExtensions
+    {
+        /// <summary>
+        /// Sets the <see cref="GrpcServiceClient"/> as the client for
+        /// search service communication.
+        /// </summary>
+        /// <param name="repositoryBuilder">The <see cref="IRepositoryBuilder"/> instance.</param>
+        /// <param name="serviceAddress">Url of the gRPC search service.</param>
+        /// <param name="configure">Optional configure method.</param>
+        public static IRepositoryBuilder UseLucene29CentralizedGrpcServiceClient(this IRepositoryBuilder repositoryBuilder,
+            string serviceAddress,
+            Action<GrpcChannelOptions> configure = null)
+        {
+            var options = new GrpcChannelOptions();
+            configure?.Invoke(options);
+
+            // this channel will be disposed later, by the GrpcClientSnService class
+            var channel = GrpcChannel.ForAddress(serviceAddress, options);
+            var searchClient = new GrpcService.GrpcSearch.GrpcSearchClient(channel);
+            var serviceClient = new GrpcServiceClient(searchClient, channel);
+
+            repositoryBuilder.UseLucene29CentralizedServiceClient(serviceClient);
+
+            SnLog.WriteInformation("GrpcServiceClient set as Lucene29 Centralized Service Client.");
+
+            return repositoryBuilder;
+        }
+    }
+}
---
+++ src/SenseNet.Search.Lucene29.Centralized.GrpcClient/GrpcServiceClient.cs
@@ -0,0 +1,124 @@
+﻿using SenseNet.Search.Indexing;
+using SenseNet.Search.Lucene29.Centralized.Common;
+using SenseNet.Search.Querying;
+using System.Collections.Generic;
+using System.Linq;
+using Grpc.Net.Client;
+using static SenseNet.Search.Lucene29.Centralized.GrpcService.GrpcSearch;
+
+namespace SenseNet.Search.Lucene29.Centralized.GrpcClient
+{
+    /// <summary>
+    /// Implements the <see cref="ISearchServiceContract"/> interface and serves as a
+    /// translator between the generic sensenet types and the grpc-specific
+    /// communication classes.
+    /// </summary>
+    public class GrpcServiceClient : ISearchServiceContract
+    {
+        private readonly GrpcSearchClient _searchClient;
+        private readonly GrpcChannel _channel;
+        public GrpcServiceClient(GrpcSearchClient searchClient, GrpcChannel channel)
+        {
+            _searchClient = searchClient;
+
+            // we pin this object only to be able to shut it down properly later
+            _channel = channel;
+        }
+
+        public void ShutDown()
+        {
+            // as the channel was created on app start, we need to
+            // dispose it properly when the connection is closed
+            _channel?.Dispose();
+        }
+
+        #region ISearchServiceContract implementation
+        public void ClearIndex()
+        {
+            _searchClient.ClearIndex(new GrpcService.ClearIndexRequest());            
+        }
+        public IndexingActivityStatus ReadActivityStatusFromIndex()
+        {
+            var result = _searchClient.ReadActivityStatusFromIndex(new GrpcService.ReadActivityStatusRequest());
+
+            return new IndexingActivityStatus() 
+            { 
+                LastActivityId = result.LastActivityId,
+                Gaps = result.Gaps.ToArray()
+            };
+        }
+
+        public void WriteActivityStatusToIndex(IndexingActivityStatus state)
+        {
+            var request = new GrpcService.IndexingActivityStatus()
+            { 
+                LastActivityId = state.LastActivityId
+            };
+            request.Gaps.AddRange(state.Gaps);
+
+            _searchClient.WriteActivityStatusToIndex(request);
+        }
+
+        public void SetIndexingInfo(IDictionary<string, IndexFieldAnalyzer> analyzerTypes, IDictionary<string, IndexValueType> indexFieldTypes, IDictionary<string, string> sortFieldNames)
+        {
+            var request = new GrpcService.SetIndexingInfoRequest();
+
+            foreach (var at in analyzerTypes)            
+                request.AnalyzerTypes.Add(at.Key, (GrpcService.IndexFieldAnalyzer)at.Value);
+            foreach (var ift in indexFieldTypes)
+                request.IndexFieldTypes.Add(ift.Key, (GrpcService.IndexValueType)ift.Value);
+            foreach (var sfn in sortFieldNames)
+                request.SortFieldNames.Add(sfn.Key, sfn.Value);
+
+            _searchClient.SetIndexingInfo(request);
+        }
+
+        public void WriteIndex(SnTerm[] deletions, DocumentUpdate[] updates, IndexDocument[] additions)
+        {
+            var request = new GrpcService.WriteIndexRequest();
+            
+            request.Deletions.AddRange(deletions.Select(del => Tools.Serialize(del)));
+            request.Updates.AddRange(updates.Select(upd => Tools.Serialize(upd)));
+            request.Additions.AddRange(additions.Select(add => Tools.Serialize(add)));
+
+            _searchClient.WriteIndex(request);
+        }
+
+        public QueryResult<int> ExecuteQuery(SnQuery query, ServiceQueryContext queryContext)
+        {
+            var context = new GrpcService.ServiceQueryContext() 
+            {
+                UserId = queryContext.UserId,
+                FieldLevel = queryContext.FieldLevel                
+            };
+            context.DynamicGroups.AddRange(queryContext.DynamicGroups);
+
+            var result = _searchClient.ExecuteQuery(new GrpcService.QueryRequest()
+            {
+                Query = Tools.Serialize(query),
+                Context = context                
+            });
+
+            return new QueryResult<int>(result.Hits, result.TotalCount);
+        }
+
+        public QueryResult<string> ExecuteQueryAndProject(SnQuery query, ServiceQueryContext queryContext)
+        {
+            var context = new GrpcService.ServiceQueryContext()
+            {
+                UserId = queryContext.UserId,
+                FieldLevel = queryContext.FieldLevel
+            };
+            context.DynamicGroups.AddRange(queryContext.DynamicGroups);
+
+            var result = _searchClient.ExecuteQueryAndProject(new GrpcService.QueryRequest()
+            {
+                Query = Tools.Serialize(query),
+                Context = context
+            });
+
+            return new QueryResult<string>(result.Hits, result.TotalCount);
+        }
+        #endregion
+    }
+}
