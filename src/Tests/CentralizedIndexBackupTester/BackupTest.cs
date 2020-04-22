2e867d6150ee7d0c5e790ecd89ada160f679776e
kavics <gyeby5@gmail.com>
2020-04-22 04:14:43 +0200

    Separate Worker agent and add repository operations.

--- src/Tests/CentralizedIndexBackupTester/BackupTest.cs
+++ src/Tests/CentralizedIndexBackupTester/BackupTest.cs
@@ -1,16 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Text;
 using System.Threading;
-using SenseNet.ContentRepository;
+using System.Threading.Tasks;
 using SenseNet.ContentRepository.Search.Indexing;
-using SenseNet.ContentRepository.Search.Indexing.Activities;
-using SenseNet.ContentRepository.Storage;
-using SenseNet.Search.Indexing;
 using SenseNet.Search.Lucene29;
-using Task = System.Threading.Tasks.Task;
 
 namespace CentralizedIndexBackupTester
 {
@@ -20,31 +14,20 @@ namespace CentralizedIndexBackupTester
         private readonly TimeSpan _wait3Sec = TimeSpan.FromSeconds(3);
         private readonly TimeSpan _wait5Sec = TimeSpan.FromSeconds(5);
 
-        private readonly string _backupDirectoryPath;
         private readonly ILuceneIndexingEngine _engine;
+        private readonly string _backupDirectoryPath; // ...this...\bin\Debug\netcoreapp3.1\App_Data\IndexBackup
 
-        // D:\projects\_fromTemplate\IndexBackup\IndexBackupTester\bin\Debug\netcoreapp3.1\App_Data\IndexBackup
         public BackupTest(ILuceneIndexingEngine engine)
         {
             _engine = engine;
             _backupDirectoryPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "App_Data", "IndexBackup");
         }
 
-        //private static readonly string TestRootName = "IndexBackupTest";
-        //private static readonly string TestRootPath = "/Root/IndexBackupTest";
         public async Task Run(CancellationToken cancellationToken)
         {
-            //// Ensure test folder
-            //var testRoot = await Node.LoadAsync<SystemFolder>(TestRootPath, cancellationToken);
-            //if (testRoot == null)
-            //{
-            //    testRoot = new SystemFolder(Repository.Root) { Name = TestRootName };
-            //    testRoot.Save();
-            //}
-
             // Start an editor worker agent
             var finisher = new CancellationTokenSource();
-            var editorTask = Work(finisher.Token);
+            var _ = new Worker().Work(finisher.Token);
 
             // Wait for backup
             await Task.Delay(_wait5Sec, cancellationToken);
@@ -57,30 +40,13 @@ namespace CentralizedIndexBackupTester
 
             // Stop worker agent
             finisher.Cancel();
-
-            // Check test result
-            Check();
-        }
-
-        private async Task Work(CancellationToken cancellationToken)
-        {
-            while (true)
-            {
-                await Task.Delay(_wait1Sec, cancellationToken);
-                if (cancellationToken.IsCancellationRequested)
-                {
-                    Console.WriteLine("Work finished");
-                    return;
-                }
-                Console.WriteLine("Work");
-            }
         }
 
         private Task Backup()
         {
-            //IndexManager.LoadCurrentIndexingActivityStatus();
-
             Console.WriteLine("Backup start");
+            Console.WriteLine("  Indexing activity status: " + IndexManager.LoadCurrentIndexingActivityStatus());
+
             var timer = Stopwatch.StartNew();
 
             _engine.BackupAsync(_backupDirectoryPath, CancellationToken.None)
@@ -91,11 +57,5 @@ namespace CentralizedIndexBackupTester
 
             return Task.CompletedTask;
         }
-
-        private void Check()
-        {
-            Console.WriteLine("Check result");
-        }
-
     }
 }
---
+++ src/Tests/CentralizedIndexBackupTester/Worker.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using SenseNet.ContentRepository;
+using SenseNet.ContentRepository.Storage;
+using SenseNet.Search;
+using Task = System.Threading.Tasks.Task;
+
+namespace CentralizedIndexBackupTester
+{
+    public class Worker
+    {
+        private readonly TimeSpan _wait1Sec = TimeSpan.FromSeconds(1);
+
+        private static readonly string TestRootName = "IndexBackupTest";
+        private static readonly string TestRootPath = "/Root/" + TestRootName;
+
+        public async Task Work(CancellationToken cancellationToken)
+        {
+            var words = ("Drop-dead date you better eat a reality sandwich before " +
+                        "you walk back in that boardroom.").Split(' ');
+
+            var testFile = await GetTestFile(cancellationToken);
+
+            for (var i = 0; ; i++)
+            {
+                await Task.Delay(_wait1Sec, cancellationToken);
+                if (cancellationToken.IsCancellationRequested)
+                {
+                    Console.WriteLine("Work finished");
+                    return;
+                }
+                Console.WriteLine("Work");
+
+                var queryResult = ContentQuery.Query($"Name:'{testFile.Name}' .AUTOFILTERS:OFF");
+                var file = queryResult.Nodes.FirstOrDefault() as File;
+                if (file == null)
+                    throw new ApplicationException("File not found: " + testFile.Path);
+
+                var text = RepositoryTools.GetStreamString(file.Binary.GetStream());
+                text += words[i % words.Length] + " ";
+
+                file.Binary.SetStream(RepositoryTools.GetStreamFromString(text));
+                file.Save();
+            }
+        }
+        private async Task<File> GetTestFile(CancellationToken cancellationToken)
+        {
+            var testRoot = await Node.LoadAsync<SystemFolder>(TestRootPath, cancellationToken);
+            if (testRoot == null)
+            {
+                testRoot = new SystemFolder(Repository.Root) { Name = TestRootName };
+                testRoot.Save();
+            }
+
+            var testFile = new File(testRoot) { Name = Guid.NewGuid().ToString() };
+            testFile.Binary.SetStream(RepositoryTools.GetStreamFromString("Start. "));
+            testFile.Save();
+
+            return testFile;
+        }
+    }
+}
