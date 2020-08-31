1f258ef92aa21491582896e67d7df7b24a8b9a19
tusmester <tusmester@gmail.com>
2020-08-31 16:01:42 +0200

    Feature/new config (#46)
    * Add Grpc client options.
    * Add validation bypass code.

--- src/SenseNet.Search.Lucene29.Centralized.GrpcClient/GrpcClientExtensions.cs
+++ src/SenseNet.Search.Lucene29.Centralized.GrpcClient/GrpcClientExtensions.cs
@@ -1,7 +1,7 @@
 ﻿using System;
+using System.Net.Http;
 using Grpc.Net.Client;
 using SenseNet.Diagnostics;
-using SenseNet.Extensions.DependencyInjection;
 using SenseNet.Search.Indexing;
 using SenseNet.Search.Lucene29.Centralized.GrpcClient;
 using SenseNet.Tools;
@@ -30,6 +30,36 @@ namespace SenseNet.Extensions.DependencyInjection
         }
 
         /// <summary>
+        /// Set the centralized Lucene engine as the search engine and
+        /// sets the <see cref="GrpcServiceClient"/> as the client for
+        /// search service communication.
+        /// </summary>
+        /// <param name="repositoryBuilder">The <see cref="IRepositoryBuilder"/> instance.</param>
+        /// <param name="options">Grpc client options</param>
+        public static IRepositoryBuilder UseLucene29CentralizedSearchEngineWithGrpc(this IRepositoryBuilder repositoryBuilder, GrpcClientOptions options)
+        {
+            //TODO: refactor the Grpc client to be able to work as a hosted service and use dependency injection correctly
+
+            // shortcut for bypassing certificate validation using a single configurable flag
+            if (!options.ValidateServerCertificate)
+            {
+                options.ChannelOptions.HttpClient = new HttpClient(new HttpClientHandler
+                {
+                    ServerCertificateCustomValidationCallback = (message, certificate2, arg3, arg4) => true
+                });
+                options.ChannelOptions.DisposeHttpClient = true;
+            }
+
+            repositoryBuilder
+                .UseLucene29CentralizedSearchEngine()
+                .UseLucene29CentralizedServiceClient(CreateGrpcServiceClient(options.ServiceAddress, options.ChannelOptions));
+
+            SnLog.WriteInformation("GrpcServiceClient set as Lucene29 Centralized Service Client.");
+
+            return repositoryBuilder;
+        }
+
+        /// <summary>
         /// Sets the <see cref="GrpcServiceClient"/> as the client for
         /// search service communication.
         /// </summary>
@@ -42,20 +72,15 @@ namespace SenseNet.Extensions.DependencyInjection
         {
             var options = new GrpcChannelOptions();
             configure?.Invoke(options);
-
-            // this channel will be disposed later, by the GrpcClientSnService class
-            var channel = GrpcChannel.ForAddress(serviceAddress, options);
-            var searchClient = new SenseNet.Search.Lucene29.Centralized.GrpcService.GrpcSearch.GrpcSearchClient(channel);
-            var serviceClient = new GrpcServiceClient(searchClient, channel);
-
-            repositoryBuilder.UseLucene29CentralizedServiceClient(serviceClient);
+            
+            repositoryBuilder.UseLucene29CentralizedServiceClient(CreateGrpcServiceClient(serviceAddress, options));
 
             SnLog.WriteInformation("GrpcServiceClient set as Lucene29 Centralized Service Client.");
 
             return repositoryBuilder;
         }
-
-        internal static SenseNet.Search.Lucene29.Centralized.GrpcService.IndexingActivityStatus ToGrpcActivityStatus(this IndexingActivityStatus state)
+        
+        internal static Search.Lucene29.Centralized.GrpcService.IndexingActivityStatus ToGrpcActivityStatus(this IndexingActivityStatus state)
         {
             var request = new SenseNet.Search.Lucene29.Centralized.GrpcService.IndexingActivityStatus
             {
@@ -64,6 +89,16 @@ namespace SenseNet.Extensions.DependencyInjection
             request.Gaps.AddRange(state.Gaps);
 
             return request;
+        }
+
+        private static GrpcServiceClient CreateGrpcServiceClient(string serviceAddress, GrpcChannelOptions options)
+        {
+            // this channel will be disposed later, by the GrpcClientSnService class
+            var channel = GrpcChannel.ForAddress(serviceAddress, options);
+            var searchClient = new Search.Lucene29.Centralized.GrpcService.GrpcSearch.GrpcSearchClient(channel);
+            var serviceClient = new GrpcServiceClient(searchClient, channel);
+
+            return serviceClient;
         }
     }
 }
---
+++ src/SenseNet.Search.Lucene29.Centralized.GrpcClient/GrpcClientOptions.cs
@@ -0,0 +1,17 @@
+﻿using Grpc.Net.Client;
+
+namespace SenseNet.Search.Lucene29.Centralized.GrpcClient
+{
+    public class GrpcClientOptions
+    {
+        public string ServiceAddress { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value that may be used by the app start process to set a certificate validation
+        /// callback that skips validation.
+        /// Default value is true. Do not set this to false unless in a development environment.
+        /// </summary>
+        public bool ValidateServerCertificate { get; set; } = true;
+        public GrpcChannelOptions ChannelOptions { get; set; } = new GrpcChannelOptions();
+    }
+}
