90ca6466ea6dd4fe8ef10cd3f0f0cd705b83a04f
tusmester <tusmester@gmail.com>
2020-08-31 13:27:48 +0200

    Add Grpc client options.

---
+++ src/SenseNet.Search.Lucene29.Centralized.GrpcClient/GrpcClientOptions.cs
@@ -0,0 +1,10 @@
+﻿using Grpc.Net.Client;
+
+namespace SenseNet.Search.Lucene29.Centralized.GrpcClient
+{
+    public class GrpcClientOptions
+    {
+        public string ServiceAddress { get; set; }
+        public GrpcChannelOptions ChannelOptions { get; set; } = new GrpcChannelOptions();
+    }
+}
